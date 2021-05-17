4c26046568a6d9019ee4631924c554483aac2f08
Felipe David <felipedavid.huh@gmail.com>
2021-05-17 13:31:52 -0300

    '.'
    Former-commit-id: 4c4d6c027635efbafb40ac8749dab59ca1b17687

---
+++ src/signal_event_manager.c
@@ -0,0 +1,8 @@
+static const uint32_t SIGNAL_EVENT_FUN_PTR = 0x00703F76;
+static const uint32_t SIGNAL_EVENT_NO_PARAMS_FUN_PTR = 0x00703E72;
+
+void initialize_signal_event_hook_no_args() {
+}
+
+void initialize_signal_event_hook() {
+}
