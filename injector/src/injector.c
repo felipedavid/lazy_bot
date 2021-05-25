1496391e360f5f3f3d1dab30a3a9c3b8dc72210d
Felipe David <felipedavid.huh@gmail.com>
2021-05-25 15:03:29 -0300

    organizing stuff
    Former-commit-id: 6dc460d33313c154c089ad0ff899c0a92dbec952

--- injector/src/injector.c
+++ injector/src/injector.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define DLL_PATH "C:\\lazy\\bin\\lazy_bot.dll"
+#define DLL_PATH "W:\\lazy\\bin\\lazy_bot.dll"
 
 void set_debug_privileges() {
     LUID luid;
