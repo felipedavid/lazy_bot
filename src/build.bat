61e80a6e0013b437b65971b79f5e579f5ab47384
Felipe David <felipedavid.huh@gmail.com>
2021-07-09 18:07:26 -0300

    moving injector around
    Former-commit-id: eb847438f721351f15aa845d9d5c41b0cfe2a75a

--- src/build.bat
+++ src/build.bat
@@ -2,3 +2,4 @@ if not exist ..\bin\ (
     mkdir ..\bin\
 )
 cl.exe /Zi /LD /EHsc main.cpp game\position.cpp bot\bot.cpp utils\sync_thread.cpp utils\hacks.cpp game\objects\object.cpp game\objects\unit.cpp game\objects\local_player.cpp game\object_manager.cpp  game\functions.cpp utils\memory_manager.cpp gui\imgui*.cpp gui\gui.cpp -o ..\bin\lazy_bot.dll /link user32.lib d3d9.lib 
+cl.exe injector\injector.cpp user32.lib advapi32.lib -o ..\bin\injector.exe
