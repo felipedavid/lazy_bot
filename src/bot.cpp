f5640df30963d4d42260f9282753467d7ee4af31
Felipe David <felipedavid.huh@gmail.com>
2021-09-24 09:07:08 -0300

    .

--- src/bot.cpp
+++ src/bot.cpp
@@ -1,27 +1,25 @@
 #include <windows.h>
 
 #include "bot.h"
-#include "gui\hook.h"
 
 void Bot::toggle_running_state() {
     running = !running;
 }
 
 void Bot::print_visible_objects() {
-    get_end_scene_ptr();
-    //entity_manager.populate_lists();
+    entity_manager.populate_lists();
 
-    //system("cls");
-    //printf("Player list:");
-    //int i = 1;
-    //for (auto unit : entity_manager.units) {
-    //    printf("Unit %d\nName: %s\nGuid: %llu\nPointer: %u\nHealth: %d\n Distance: %.1f\n\n", 
-    //            i, unit.get_name(), unit.get_guid(), unit.base_addr, unit.get_health(), 
-    //            entity_manager.local_player.distance_to(unit.get_position()));
-    //    i++;
-    //}
-    //printf("Local player:");
-    //auto player = entity_manager.local_player;
-    //printf("Unit %d\nName: %s\nGuid: %llu\nPointer: %u\nHealth: %d\n\n", 
-    //        i, player.get_name(), player.get_guid(), player.base_addr, player.get_health());
+    system("cls");
+    printf("Player list:");
+    int i = 1;
+    for (auto unit : entity_manager.players) {
+        printf("Unit %d\nName: %s\nGuid: %llu\nPointer: %u\nHealth: %d\n Distance: %.1f\n\n", 
+                i, unit.get_name(), unit.get_guid(), unit.base_addr, unit.get_health(), 
+                entity_manager.local_player.distance_to(unit.get_position()));
+        i++;
+    }
+    printf("Local player:");
+    auto player = entity_manager.local_player;
+    printf("Unit %d\nName: %s\nGuid: %llu\nPointer: %u\nHealth: %d\n\n", 
+            i, player.get_name(), player.get_guid(), player.base_addr, player.get_health());
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,18 @@
 #include <cstdio>
 
 #include "bot.h"
+#include "gui\gui.h"
 
 void entry_point(HMODULE instance) {
     FILE *dummy_file;
     AllocConsole();
     freopen_s(&dummy_file, "CONOUT$", "w", stdout);
 
-    Bot bot;
-    while (true) {
-        if (GetAsyncKeyState('M')) {
-            bot.print_visible_objects();
-        }
+    start_gui(instance);
 
-        if (GetAsyncKeyState(VK_ESCAPE)) {
-            fclose(dummy_file);
-            FreeConsole();
-            FreeLibraryAndExitThread(instance, 0);
-        }
-        Sleep(100);
-    }
+    fclose(dummy_file);
+    FreeConsole();
+    FreeLibraryAndExitThread(instance, 0);
 }
 
 BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved )
