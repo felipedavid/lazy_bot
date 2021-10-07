3ec689774e3806de759fe41e04c26cd4dc252a9c
Felipe David <felipedavid.huh@gmail.com>
2021-10-07 09:26:57 -0300

    fixing wrong address

--- lazy_bot/hacks.cpp
+++ lazy_bot/hacks.cpp
@@ -2,5 +2,5 @@
 
 void unlock_lua() {
     char patch[] = { 0xB8, 0x01, 0x00, 0x00, 0x00, 0xc3 };
-    write_to_memory((char *)0x860A90, (char *)patch, 6);
+    write_to_memory((char *)0x494A50, (char *)patch, 6);
 }
